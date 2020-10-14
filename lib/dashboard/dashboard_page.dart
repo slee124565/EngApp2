0de039f8fe0acf411552ab381b7dff6ee3bea237
Lee Shiueh <lee.shiueh@gmail.com>
2020-10-14 18:07:23 +0800

    BottomNavigationBar devp

--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   static String id = 'dashboard_page';
 
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,6 +59,43 @@ class DashboardPage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        unselectedIconTheme: IconThemeData(
+          color: Color(0xFF777472),
+        ),
+        unselectedLabelStyle: GoogleFonts.notoSans(
+          fontSize: 10.0,
+          fontWeight: FontWeight.w600,
+          fontStyle: FontStyle.normal,
+          color: Color(0xFF777472),
+        ),
+        selectedIconTheme: IconThemeData(
+            color: Color(0xFFB8A85A),
+        ),
+        items: const<BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label:'首頁'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:'房間'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:'服務'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:'通知'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:'個人'
+          ),
+        ],
       ),
     );
   }
